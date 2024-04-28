46f3de41715ad0b821e0d3edd1903eeab593ad41
DarshilModi29 <132668473+DarshilModi29@users.noreply.github.com>
2024-04-28 18:57:03 +0530

    Update News, Navbar and NewsItem components; add InfiniteScroll and LoadingBar; remove PropTypes and defaultProps
    The changes include:
    - updating the News component to use useCallback for the update function, and removing PropTypes and default

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import Navbar from './components/Navbar';
 import News from './components/News';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar';
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 export default function App() {
   const pageSize = 9;
   const apiKey = process.env.REACT_APP_NEWS_API;
 
   const [progress, setProgress] = useState(0)
-  // setProgress(progress);
-    return (
-      <>
+  return (
+    <>
       <BrowserRouter>
         <Navbar />
         <LoadingBar
@@ -20,16 +19,16 @@ export default function App() {
           progress={progress}
         />
         <Routes>
-          <Route exact path="/" element={<News prog={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}></Route>
-          <Route exact path="/business" element={<News prog={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/>}></Route>
-          <Route exact path="/health" element={<News prog={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health"/>}></Route>
-          <Route exact path="/entertainment" element={<News prog={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}></Route>
-          <Route exact path="/science" element={<News prog={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"/>}></Route>
-          <Route exact path="/sports" element={<News prog={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/>}></Route>
-          <Route exact path="/technology" element={<News prog={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/>}></Route>
-          
+          <Route exact path="/" element={<News prog={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />}></Route>
+          <Route exact path="/business" element={<News prog={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />}></Route>
+          <Route exact path="/health" element={<News prog={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />}></Route>
+          <Route exact path="/entertainment" element={<News prog={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />}></Route>
+          <Route exact path="/science" element={<News prog={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />}></Route>
+          <Route exact path="/sports" element={<News prog={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />}></Route>
+          <Route exact path="/technology" element={<News prog={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />}></Route>
+
         </Routes>
       </BrowserRouter>
-      </>
-    )
+    </>
+  )
 }
--- src/components/spinner.js
+++ src/components/spinner.js
@@ -4,9 +4,9 @@ import loading from './loading.gif'
 export default function Spinner() {
   return (
     <>
-    <div className='text-center'>
-          <img src={loading} alt="loading" height={90} width={90}  />
-    </div>
+      <div className='text-center'>
+        <img src={loading} alt="loading" height={90} width={90} />
+      </div>
     </>
   )
 }
